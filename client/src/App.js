188cd0af890425694f6bd9db5542df108feef880
Akshit Chaturvedi <akshitc2228@gmail.com>
2023-08-13 18:42:30 +0100

    feat: Add routes, outlets, etc for other pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,75 @@
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
+import NavBar from "./components/navBar/NavBar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import {
   createBrowserRouter,
   RouteProvider,
   Route,
   RouterProvider,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
 
 function App() {
+  const currUser = false;
+
+  const Layout = () => {
+    return (
+      <div>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
       path: "/login",
-      element: <Login/>
+      element: <Login />,
     },
     {
       path: "/register",
-      element: <Register/>
-    }
-  ])
+      element: <Register />,
+    },
+  ]);
 
   return (
     <div>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <span>I say something</span>
+    </div>
+  )
+}
+
+export default Home
